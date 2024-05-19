8d8cbc01104ef7a7d384ec095c7e7e761e7a1b58
GytisMalinauskas <159523970+GytisMalinauskas@users.noreply.github.com>
2024-05-19 17:30:07 +0300

    Add files via upload

---
+++ vector/headers/function.h
@@ -0,0 +1,34 @@
+#ifndef function_h
+#define function_h
+#include <cstdio>
+#include <locale>
+#include <time.h>
+#include <limits>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <cstring> 
+#include <cerrno>
+#include <random>
+#include <chrono>
+#include "../headers/studentas.h"
+#include "../headers/error.h"
+
+void Skaicius1(vector<studentas>& s, int &m, int skaicius, int MinP, int MaxP);
+void Skaicius2(vector<studentas>& s, int skaicius, int MinP, int MaxP);
+void FailoSkaitymas(vector <studentas> &s, string &duomPav);
+void VarduGeneravimui();
+void Sorting(vector<studentas>& s,vector<studentas> &k, vector <studentas> &l, string choice, int rikiavimas, int strategy);
+void Isvedimas(vector <studentas> s, vector <studentas> &l, vector <studentas> &k);
+bool rikiavimasVid(studentas& s1, studentas& s2);
+bool rikiavimasMed(studentas& s1, studentas& s2);
+bool isBelowMedian(const studentas& stud);
+bool isBelowAverage(const studentas& stud);
+void GenerateFile (int kiekis, int nd_kiekis, string &fileName);
+#endif
---
+++ vector/headers/main.h
@@ -0,0 +1,9 @@
+#ifndef main_h
+#define main_h
+#include <iostream>
+#include "../headers/studentas.h"
+#include <cerrno>
+#include "../headers/error.h"
+#include "../headers/function.h"
+
+#endif
