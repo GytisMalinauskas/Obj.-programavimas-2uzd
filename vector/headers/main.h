54b98b29d757c58e6243531bc06183ef4425626a
GytisMalinauskas <159523970+GytisMalinauskas@users.noreply.github.com>
2024-05-20 20:14:45 +0300

    Add files via upload

---
+++ vector/headers/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define main_h
+#include <iostream>
+#include <fstream>
+#include "../headers/studentas.h"
+#include <cerrno>
+#include "../headers/error.h"
+#include "../headers/function.h"
+
+#endif
